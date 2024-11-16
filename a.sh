86176a07df8a9c1d018607fa04995d1764d51ee5
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2024-11-17 02:45:20 +0700

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -L https://raw.githubusercontent.com/nezhahq/scripts/main/install_en.sh -o nezha.sh
+chmod +x nezha.sh
+./nezha.sh install_agent vps1.dualeovpn.net 5555 hQetwAbZqMaJYzkZJz
